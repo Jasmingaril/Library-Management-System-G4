4cadc9deabe302d2e4c538db55491e761b3334f5
Jasmingaril <gariljasmin@gmail.com>
2025-05-19 13:54:25 +0800

    create

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,60 +1,12 @@
 "use client";
 
 import React from "react";
-import Sidebar from "./dashboard/sidebar"; // Sidebar component
-import Navbar from "./dashboard/navbar"; // Navbar component
-import CreateAccount from "./create-account/page"; 
+import Sidebar from "./dashboard/sidebar";
+import Navbar from "./dashboard/navbar";
+import CreateAccount from "./create-account/page";
 
 const Home: React.FC = () => {
   return (
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // Main layout
     <div className="flex flex-col h-screen">
       {/* Navbar */}
       <Navbar />
@@ -62,6 +14,34 @@ const Home: React.FC = () => {
       <div className="flex flex-1">
         {/* Sidebar */}
         <Sidebar />
+
+        {/* Main Content */}
+        <main className="flex-1 p-6 bg-gray-50 dark:bg-gray-900">
+          <div className="h-full max-w-4xl mx-auto overflow-hidden bg-white rounded-lg shadow-xl dark:bg-gray-800 flex flex-col md:flex-row">
+            <div className="h-32 md:h-auto md:w-1/2">
+              <img
+                aria-hidden="true"
+                className="object-cover w-full h-full dark:hidden"
+                src="/assets/img/create-account-office.jpeg"
+                alt="Office"
+              />
+              <img
+                aria-hidden="true"
+                className="hidden object-cover w-full h-full dark:block"
+                src="/library.png/"
+                alt="Office"
+              />
+            </div>
+            <div className="flex items-center justify-center p-6 sm:p-12 md:w-1/2 w-full">
+              <div className="w-full">
+                <h1 className="mb-4 text-xl font-semibold text-gray-700 dark:text-gray-200">
+                  Create account
+                </h1>
+                <CreateAccount />
+              </div>
+            </div>
+          </div>
+        </main>
       </div>
     </div>
   );

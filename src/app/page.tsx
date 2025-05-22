09c249c02e20e8b1b0cf9c56f8a28180f2a099e6
jhonamaebobier <bobierjhona@gmail.com>
2025-05-22 15:43:46 +0800

    remove unnecessary code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,12 @@
 "use client";
 
 import React from "react";
-import Sidebar from "./dashboard/sidebar";
 import Navbar from "./dashboard/navbar";
-import CreateAccount from "./create-account/page";
 
 const Home: React.FC = () => {
   return (
     <div className="flex flex-col h-screen">
-      {/* Navbar */}
       <Navbar />
-
-      <div className="flex flex-1">
-      </div>
     </div>
   );
 };
